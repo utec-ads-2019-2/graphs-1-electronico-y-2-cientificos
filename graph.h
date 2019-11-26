567daee24a3ed2c34b99d69ea87ef58652c564ab
alejandro <albino.mamani@utec.edu.pe>
2019-11-26 11:44:08 -0500

    Remaking Floyd

--- graph.h
+++ graph.h
@@ -90,8 +90,6 @@ public:
     bool removeEdge(N from, N to);
 
     self& primMST(N start);
-
-    pair<unordered_map<typename Graph<Tr>::N, unordered_map<typename Graph<Tr>::N, typename Graph<Tr>::E>>, unordered_map<typename Graph<Tr>::N, unordered_map<typename Graph<Tr>::N, typename Graph<Tr>::N>>> floyd();
     
 };
 
@@ -427,54 +425,7 @@ Graph<Tr> & Graph<Tr>::primMST(N source)
 }
 
 
-template <typename Tr>
-pair<unordered_map<typename Graph<Tr>::N, unordered_map<typename Graph<Tr>::N, typename Graph<Tr>::E>>, unordered_map<typename Graph<Tr>::N, unordered_map<typename Graph<Tr>::N, typename Graph<Tr>::N>>> Graph<Tr>::floyd()
-{
-    unordered_map<N, unordered_map<N,E>> dist;
-    unordered_map<N, unordered_map<N,N>> parent;
-    E inf = numeric_limits<E>::max();
-    for(auto it : nodes)
-    {
-        for(auto jt : nodes)
-        {
-        dist[it.first][jt.first]=inf;
-        parent[it.first][jt.first]=jt.first;
-            if(it.first == jt.first)
-                {
-                    dist[it.first][jt.first]=0;
-                }
 
-        }
-    }
-    for(auto it: nodes)
-    {
-        for(edge* edg: it.second->edges)
-        {
-            N s = edg->nodes[1]->get_data();
-            dist[it.first][s]= edg->get_data();
-            parent[it.first][s]= s;
-        }
-    }
-    for(auto ik: nodes)
-    {
-        N k = ik.first;
-        for(auto ii: nodes)
-        {
-            N i = ii.first;
-            for(auto ij: nodes)
-            {
-                N j = ij.first;
-                E temp = dist[i][k] + dist[k][j];
-                if(temp < dist[i][j])
-                {
-                    dist[i][j]=temp;
-                    parent[i][j]=k;
-                }
-            }
-        }
-    }
-    return make_pair(dist,parent);
-}
 
 typedef Graph<Traits> graph;
 
